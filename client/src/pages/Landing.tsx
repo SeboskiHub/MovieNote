8412158506380b07804bf926ef50fbca8b5a4cba
Camila Holguín <camilaholguinmrpo@gmail.com>
2026-04-14 21:12:04 -0500

    Create landing page structure

---
+++ client/src/pages/Landing.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom"; // Hook que permite navegar entre rutas sin recargar la página
+
+export function Landing() { // Componente principal de la pantalla inicial
+
+  const navigate = useNavigate(); // Creamos la función que nos permite redirigir a otras rutas
+
+  return (
+    <div className="landing"> {}
+
+      <h1> MovieNote </h1> {}
+
+      <p>Las películas no solo se ven, se sienten.
+        Movie Note te permite guardar cada emoción para que puedas volver a ellas cuando lo necesites.</p> {}
+
+      <button
+        onClick={() => navigate("/login")} // Cuando el usuario hace click → lo llevamos al login
+      >
+        Empezar
+      </button>
+
+    </div>
+  );
+}
