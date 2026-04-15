0e3d5e4327d8a45364d1f54753bf4c245289b165
Camila Holguín <camilaholguinmrpo@gmail.com>
2026-04-15 01:08:11 -0500

    Add reusable emotions list

---
+++ client/src/utils/Emotions.ts
@@ -0,0 +1,35 @@
+
+// Definimos un arreglo de emociones reutilizable en toda la app
+
+export const emotions = [
+  {
+    id: 1, 
+    name: "Happy", 
+    emoji: "😊",
+  },
+  {
+    id: 2,
+    name: "Sad",
+    emoji: "😢",
+  },
+  {
+    id: 3,
+    name: "Excited",
+    emoji: "🔥",
+  },
+  {
+    id: 4,
+    name: "Scared",
+    emoji: "☠️",
+  },
+  {
+    id: 5,
+    name: "Inspired",
+    emoji: "✨",
+  },
+  {
+    id: 6,
+    name: "Confused",
+    emoji: "🤯",
+  },
+];
